dbae1d7f5b1b81cd27438e693eb43d43dd267888
Tim Szewczyk <tim.szewczyk@gmail.com>
2018-03-13 22:58:29 -0600

    vectorized operations

--- code/fn_sim.R
+++ code/fn_sim.R
@@ -4,44 +4,50 @@
 
 ##--- simulate expected values
 ## 
-sim_expected <- function(d_i, d.k, e.k, E, p, X, n_z) {
-  size.mx <- with(d_i[d.k,], cbind(1, size, size^2, size^3))
-  E$s[e.k] <- antilogit(size.mx[,1:n_z$s] %*% p$s_z + c(X$s %*% p$s_x))
-  E$g[e.k] <- size.mx[,1:n_z$g] %*% p$g_z + c(X$g %*% p$g_x)
-  E$fl[e.k] <- antilogit(size.mx[,1:n_z$fl] %*% p$fl_z + c(X$fl %*% p$fl_x))
-  E$seed[e.k] <- exp(size.mx[,1:n_z$seed] %*% p$seed_z + c(X$seed %*% p$seed_x))
+sim_expected <- function(k, z.i, e.k, E, p, X, n_z) {
+  if(length(z.i) > 0) {
+    z.mx <- cbind(1, z.i, z.i^2, z.i^3)
+    E$yr[e.k] <- k
+    E$size[e.k] <- z.i
+    E$s[e.k] <- antilogit(z.mx[,1:n_z$s] %*% p$s_z + c(X$s %*% p$s_x))
+    E$g[e.k] <- z.mx[,1:n_z$g] %*% p$g_z + c(X$g %*% p$g_x)
+    E$fl[e.k] <- antilogit(z.mx[,1:n_z$fl] %*% p$fl_z + c(X$fl %*% p$fl_x))
+    E$seed[e.k] <- exp(z.mx[,1:n_z$seed] %*% p$seed_z + c(X$seed %*% p$seed_x))
+  }
   return(E)
 }
 
 
 ##--- simulate realized values
 ##
-sim_realized <- function(d_i, d.k, e.k, E, p, lo, hi) {
+sim_realized <- function(k, z.i, d_i, d.k, e.k, E, p, lo, hi) {
   n_t <- length(d.k)
-  d_i$surv[d.k] <- rbinom(n_t, 1, E$s[e.k])
-  d_i$sizeNext[d.k] <- rnorm(n_t, E$g[e.k], p$g_sig) * 
-    ifelse(d_i$surv[d.k], 1, NA)
-  d_i$sizeNext[d.k] <- ifelse(d_i$sizeNext[d.k] < lo, 
-                                  lo, d_i$sizeNext[d.k])
-  d_i$sizeNext[d.k] <- ifelse(d_i$sizeNext[d.k] > hi, 
-                                  hi, d_i$sizeNext[d.k])
-  d_i$fl[d.k] <- rbinom(n_t, 1, E$fl[e.k]) * ifelse(d_i$surv[d.k], 1, NA)
-  d_i$seed[d.k] <- rpois(n_t, E$seed[e.k]) * 
-    ifelse(d_i$surv[d.k] & d_i$fl[d.k], 1, NA)
+  if(n_t > 0) {
+    d_i$yr[d.k] <- k
+    d_i$size[d.k] <- z.i
+    d_i$surv[d.k] <- rbinom(n_t, 1, E$s[e.k])
+    surv.ik <- ifelse(surv.ik, 1, NA)
+    d_i$sizeNext[d.k] <- rnorm(n_t, E$g[e.k], p$g_sig) * surv.ik
+    d_i$sizeNext[d.k] <- pmax(d_i$sizeNext[d.k], lo)
+    d_i$sizeNext[d.k] <- pmin(d_i$sizeNext[d.k], hi)
+    d_i$fl[d.k] <- fl.ik <- rbinom(n_t, 1, E$fl[e.k]) * surv.ik
+    d_i$seed[d.k] <- rpois(n_t, E$seed[e.k]) * ifelse(fl.ik, 1, NA)
+  }
   return(d_i)
 }
 
 
 ##--- simulate number of recruits
 ##
-sim_recruits <- function(d_i, p_est_ik, nSd_ik, B_ik, D_ik, p) {
+sim_recruits <- function(k, d_i, p_est_ik, nSd_ik, B_ik, D_ik, p) {
   n <- round(p_est_ik * (nSd_ik * (1-p$p_emig) * p$rcr_dir + 
                            B_ik * p$rcr_SB + 
                            D_ik * p$rcr_dir)) 
   if(n > 0) {
-    n_index <- (1:n)+nrow(d_i)
-    d_i[n_index, "sizeNext"] <- rnorm(n, p$rcr_z[1], p$rcr_z[2])
-    d_i[n_index, "yr"] <- max(d_i$yr, na.rm=T)
+    rcr_i <- (1:n)+length(d_i$yr)
+    d_i$sizeNext[rcr_i] <- rnorm(n, p$rcr_z[1], p$rcr_z[2])
+    d_i$yr[rcr_i] <- k
+    d_i$size[rcr_i] <- d_i$surv[rcr_i] <- d_i$fl[rcr_i] <- d_i$seed[rcr_i] <- NA
   }
   return(d_i)
 }
@@ -50,57 +56,50 @@ sim_recruits <- function(d_i, p_est_ik, nSd_ik, B_ik, D_ik, p) {
 ##--- simulate addition to seed bank
 ##
 sim_seedbank <- function(nSd_ik, B_ik, D_ik, p) {
-  p$s_SB * (B_ik * (1-p$rcr_SB) + 
-              nSd_ik * (1-p$p_emig) * (1-p$rcr_dir) + 
-              D_ik * (1-p$rcr_dir))
+  round(p$s_SB * (B_ik * (1-p$rcr_SB) + 
+                    nSd_ik * (1-p$p_emig) * (1-p$rcr_dir) + 
+                    D_ik * (1-p$rcr_dir)))
 }
 
 
 ##--- simulate full dataset
 ##
-simulate_data <- function(n.cell, tmax, n_i, z.rng, lo, hi, 
+simulate_data <- function(n.cell, tmax, n0, z.rng, lo, hi, 
                           p, X, n_z, sdd, sdd.j, verbose=FALSE) {
   require(tidyverse)
+  i <- 1:n.cell
   
   # storage objects
-  E <- d <- map(1:n.cell, ~data.frame(NULL))
-  B <- cbind(rpois(n.cell, n_i), matrix(0, nrow=n.cell, ncol=tmax))
-  nSd <- D <- p_est.i <- N_sim <- matrix(0, nrow=n.cell, ncol=tmax)
+  E <- d <- map(i, ~list())  # much faster than using dataframes
+  B <- cbind(rpois(n.cell, n0), matrix(0, nrow=n.cell, ncol=tmax))
+  nSd <- D <- N_sim <- matrix(0, nrow=n.cell, ncol=tmax)
+  p_est.i <- matrix(p$p_est, nrow=n.cell, ncol=tmax)
+  z.k <- map(i, ~runif(n0, z.rng[1], z.rng[2]))
+  X_map <- lapply(i, function(x) map(X, ~.[x,]))
   
   for(k in 1:tmax) {
     ## local growth
-    for(i in 1:n.cell) {
-      if(k==1) {
-        size.k <- runif(n=n_i[i], min=z.rng[1], max=z.rng[2])
-      } else {
-        size.k <- d[[i]]$sizeNext[!is.na(d[[i]]$sizeNext) & d[[i]]$yr == k-1]
-      }
-      d.k <- (1:length(size.k)) + nrow(d[[i]])
-      e.k <- (1:length(size.k)) + nrow(E[[i]])
-      if(length(size.k) > 0) {
-        E[[i]][e.k, "yr"] <- d[[i]][d.k, "yr"] <- k
-        E[[i]][e.k, "size"] <- d[[i]][d.k, "size"] <- size.k
-        E[[i]] <- sim_expected(d[[i]], d.k, e.k, E[[i]], p, map(X, ~.[i,]), n_z)
-        d[[i]] <- sim_realized(d[[i]], d.k, e.k, E[[i]], p, lo, hi)
-        nSd[i,k] <- sum(d[[i]]$seed[d.k], na.rm=T)
-        N_sim[i,k] <- length(size.k)
-      }
-      sdd[,,3,i] <- nSd[i,k] * p$p_emig * sdd[,,1,i]
-    }
+    if(k>1) z.k <- map(d, ~.$sizeNext[!is.na(.$sizeNext) & .$yr == k-1])
+    d.k <- lapply(i, function(x) seq_along(z.k[[x]]) + nrow(d[[x]]))
+    e.k <- lapply(i, function(x) seq_along(z.k[[x]]) + nrow(E[[x]]))
+    E <- lapply(i, function(x) sim_expected(k, z.k[[x]], e.k[[x]], E[[x]], 
+                                            p, X_map[[x]], n_z))
+    d <- lapply(i, function(x) sim_realized(k, z.k[[x]], d[[x]], d.k[[x]], 
+                                            e.k[[x]], E[[x]], p, lo, hi))
+    N_sim[,k] <- vapply(z.k, length, 1)
+    nSd[,k] <- vapply(i, function(x) sum(d[[x]]$seed[d.k[[x]]], na.rm=TRUE), 1)
+    
     ## dispersal & density dependence
-    for(i in 1:n.cell) {
-      D[i,k] <- sum(sdd[,,3,][sdd.j[[i]]])
-      p_est.i[i,k] <- ifelse(p$NDD, min(p$NDD_n/(nSd[i,k]+D[i,k]), p$p_est), 
-                             p$p_est)
-      d[[i]] <- sim_recruits(d[[i]], p_est.i[i,k], nSd[i,k], B[i,k], D[i,k], p)
-      B[i,k+1] <- sim_seedbank(nSd[i,k], B[i,k], D[i,k], p)
-    }
+    sdd.D <- vapply(i, function(x) nSd[x,k]*p$p_emig*sdd[,,1,x], sdd[,,1,1])
+    D[,k] <- vapply(sdd.j, function(x) sum(sdd.D[x]), 1)
+    if(p$NDD) p_est.i[,k] <- pmin(p$NDD_n/(nSd[,k]+D[,k]), p$p_est)
+    d <- lapply(i, function(x) sim_recruits(k, d[[x]], p_est.i[x,k], nSd[x,k], 
+                                            B[x,k], D[x,k], p))
+    B[,k+1] <- vapply(i, function(x) sim_seedbank(nSd[x,k], B[x,k], D[x,k],p),1)
     if(verbose) cat("Finished", n.cell, "cells for year", k, "\n")
   }
-  
   return(list(E=E, d=d, B=B, nSd=nSd, D=D, p_est.i=p_est.i, N_sim=N_sim))
 }
-
 
 
 
