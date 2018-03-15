eada678f618ff54528c8267ae8939176f3333e4a
Tim Szewczyk <tim.szewczyk@gmail.com>
2018-03-15 10:49:32 -0600

    reformatting for clarity

--- code/fn_aux.R
+++ code/fn_aux.R
@@ -68,25 +68,25 @@ summarize_IPM_simulations <- function(S.i) {
                 D=map(S.i, ~.$D),
                 p_est=map(S.i, ~.$p_est.i),
                 N_sim=map(S.i, ~.$N_sim),
-                N_tot=map(S.i, ~map_dbl(.$d, 
-                                        ~sum(!is.na(.$sizeNext[.$yr==tmax])))),
-                N_surv=map(S.i, ~map_dbl(.$d, 
-                                         ~sum(.$surv[.$yr==tmax], na.rm=T))),
-                N_rcr=map(S.i, ~map_dbl(.$d, 
-                                        ~sum(is.na(.$size[.$yr==tmax]))))) %>% 
+                N_tot=map(S.i, 
+                          ~map_dbl(.$d, ~sum(!is.na(.$sizeNext[.$yr==tmax])))),
+                N_surv=map(S.i, 
+                           ~map_dbl(.$d, ~sum(.$surv[.$yr==tmax], na.rm=T))),
+                N_rcr=map(S.i, 
+                          ~map_dbl(.$d, ~sum(is.na(.$size[.$yr==tmax]))))) %>% 
     map(simplify2array)
   rm(S.i)
   
-  return(list(B.mn=apply(S.sum$B, c(1,2), mean), 
-              B.sd=apply(S.sum$B, c(1,2), sd), 
-              nSd.mn=apply(S.sum$nSd, c(1,2), mean), 
-              nSd.sd=apply(S.sum$nSd, c(1,2), sd), 
-              D.mn=apply(S.sum$D, c(1,2), mean), 
-              D.sd=apply(S.sum$D, c(1,2), sd), 
-              p_est.mn=apply(S.sum$p_est, c(1,2), mean), 
-              p_est.sd=apply(S.sum$p_est, c(1,2), sd), 
-              N_sim.mn=apply(S.sum$N_sim, c(1,2), mean), 
-              N_sim.sd=apply(S.sum$N_sim, c(1,2), sd),
+  return(list(B.mn=apply(S.sum$B, 1:2, mean), 
+              B.sd=apply(S.sum$B, 1:2, sd), 
+              nSd.mn=apply(S.sum$nSd, 1:2, mean), 
+              nSd.sd=apply(S.sum$nSd, 1:2, sd), 
+              D.mn=apply(S.sum$D, 1:2, mean), 
+              D.sd=apply(S.sum$D, 1:2, sd), 
+              p_est.mn=apply(S.sum$p_est, 1:2, mean), 
+              p_est.sd=apply(S.sum$p_est, 1:2, sd), 
+              N_sim.mn=apply(S.sum$N_sim, 1:2, mean), 
+              N_sim.sd=apply(S.sum$N_sim, 1:2, sd),
               N_tot.mn=apply(S.sum$N_tot, 1, mean),
               N_tot.sd=apply(S.sum$N_tot, 1, sd),
               N_surv.mn=apply(S.sum$N_surv, 1, mean),
@@ -99,3 +99,4 @@ summarize_IPM_simulations <- function(S.i) {
 ##-- antilogit
 antilogit <- function (x) {
   exp(x)/(1 + exp(x))
+}
