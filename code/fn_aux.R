1e96add1b9a0c0c3058144609f11fb68f9e690e5
Tim Szewczyk <tim.szewczyk@gmail.com>
2018-03-15 15:16:49 -0600

    function for summarizing IPM output from multiple samples

--- code/fn_aux.R
+++ code/fn_aux.R
@@ -75,7 +75,6 @@ summarize_IPM_simulations <- function(S.i, tmax) {
                 N_rcr=map(S.i, 
                           ~map_dbl(.$d, ~sum(is.na(.$size[.$yr==tmax]))))) %>% 
     map(simplify2array)
-  rm(S.i)
   
   return(list(B.mn=apply(S.sum$B, 1:2, mean), 
               B.sd=apply(S.sum$B, 1:2, sd), 
@@ -93,6 +92,42 @@ summarize_IPM_simulations <- function(S.i, tmax) {
               N_surv.sd=apply(S.sum$N_surv, 1, mean),
               N_rcr.mn=apply(S.sum$N_rcr, 1, mean),
               N_rcr.sd=apply(S.sum$N_rcr, 1, mean)))
+}
+
+
+##-- calculate means from simulation means of multiple IPM samples
+summarize_IPM_samples <- function(U.f, S.f) {
+  Ua <- list(IPM.mn=map(U.f, ~.$IPMs),
+             P.mn=map(U.f, ~.$Ps),
+             F.mn=map(U.f, ~.$Fs),
+             Nt.mn=map(U.f, ~.$Nt),
+             lam.mn=map(U.f, ~.$lam.t),
+             p_est.mn=map(U.f, ~.$p_est.t)) %>% 
+    map(simplify2array)
+  Uf <- list(IPM.mn=apply(Ua$IPM.mn, 1:3, mean),
+             P.mn=apply(Ua$P.mn, 1:3, mean),
+             F.mn=apply(Ua$F.mn, 1:3, mean),
+             Nt.mn=apply(Ua$Nt.mn, 1:3, mean),
+             lam.mn=apply(Ua$lam.mn, 1:2, mean),
+             p_est.mn=apply(Ua$p_est.mn, 1:2, mean))
+  Sa <- list(B.mn=map(S.f, ~.$B.mn),
+             nSd.mn=map(S.f, ~.$nSd.mn),
+             D.mn=map(S.f, ~.$D.mn),
+             p_est.mn=map(S.f, ~.$p_est.mn),
+             N_sim.mn=map(S.f, ~.$N_sim.mn),
+             N_tot.mn=map(S.f, ~.$N_tot.mn),
+             N_surv.mn=map(S.f, ~.$N_surv.mn),
+             N_rcr.mn=map(S.f, ~.$N_rcr.mn)) %>%
+    map(simplify2array)
+  Sf <- list(B.mn=apply(Sa$B.mn, 1:2, mean),
+             nSd.mn=apply(Sa$nSd.mn, 1:2, mean),
+             D.mn=apply(Sa$D.mn, 1:2, mean),
+             p_est.mn=apply(Sa$p_est.mn, 1:2, mean),
+             N_sim.mn=apply(Sa$N_sim.mn, 1:2, mean),
+             N_tot.mn=apply(Sa$N_tot.mn, 1, mean),
+             N_surv.mn=apply(Sa$N_surv.mn, 1, mean),
+             N_rcr.mn=apply(Sa$N_rcr.mn, 1, mean))
+  return(list(Uf=Uf, Sf=Sf))
 }
 
 
