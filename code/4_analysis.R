9cd20e6ea447e98a7dc4b83f30776a268cafb451
Sz-Tim <tim.szewczyk@gmail.com>
2018-03-16 10:17:03 -0400

    initial commit

---
+++ code/4_analysis.R
@@ -0,0 +1,11 @@
+# 4: Analyze fit models
+# Comparison of SDM approaches
+# Tim Szewczyk
+
+# This script aggregates and analyzes the output from `3_fitModels.R`.
+
+########
+## Setup
+########
+# file specifications
+sp <- "sp1"
