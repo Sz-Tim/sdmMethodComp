d2634bed7fc61e3aa727e0c59a4f4627e61c1b1d
Sz-Tim <tim.szewczyk@gmail.com>
2018-03-16 10:17:23 -0400

    initial commit

---
+++ code/5_plots.R
@@ -0,0 +1,11 @@
+# 5: Plot output
+# Comparison of SDM approaches
+# Tim Szewczyk
+
+# This script plots the output from `3_fitModels.R` and `4_analysis.R`.
+
+########
+## Setup
+########
+# file specifications
+sp <- "sp1"
