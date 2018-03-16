2b94b4794a77ebdcb23405978fd4ce2ce72f385c
Tim Szewczyk <tim.szewczyk@gmail.com>
2018-03-16 10:53:21 -0600

    idea for plot

--- code/5_plots.R
+++ code/5_plots.R
@@ -9,3 +9,8 @@
 ########
 # file specifications
 sp <- "sp1"
+
+
+# barplots showing % correct for presence/absence; fill=SDM; facet=issues
+ggplot(out_IPM, aes(x=sign(Surv.S.f) == sign(Surv.S), fill=s_Iss)) + 
+  geom_bar(position="dodge")
