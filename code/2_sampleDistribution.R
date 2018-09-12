cb8edbfae3e8944d6ff3c6a2e22a1d92062d7304
Sz-Tim <tim.szewczyk@gmail.com>
2018-09-12 15:19:26 -0600

    looping through sampling issues

--- code/2_sampleDistribution.R
+++ code/2_sampleDistribution.R
@@ -13,7 +13,7 @@
 # file specifications
 sp <- "sp1"
 overwrite <- TRUE
-sampling.issue <- c("none", "noise", "geogBias", "sampBias")[4]
+samp.issues <- c("none", "noise", "geogBias", "sampBias")
 
 # load workspace
 pkgs <- c("tidyverse", "magrittr", "here")
@@ -45,44 +45,46 @@ noise <- list(Mx=0.2, # proportion of observed presences that are false
 )
 
 
-########
-## Impose sampling bias
-########
-if(sampling.issue=="geogBias") {
-  P.pr[P.i %in% geog.excl] <- 0
-} else if(sampling.issue=="sampBias") {
-  P.pr <- P.pr * env.in$rdLen[P.i]
-}
-
-
-########
-## Draw samples
-########
-Corr.sample <- map(1:n_samp, ~sample(P.i, O_n$Corr, replace=F, prob=P.pr))
-Mech.sample <- map(1:n_samp, ~sample(P.i, O_n$Mech, replace=F, prob=P.pr))
-
-O_Mx <- map(Corr.sample, ~(1:n.cell %in% .))
-O_CA <- sample_for_CA(S, Mech.sample, O_yr, prop.sampled)
-O_IPM <- sample_for_IPM(S, Mech.sample, O_yr, prop.sampled)
-
-
-########
-## Impose sampling error
-########
-if(sampling.issue=="noise") {
-  O_Mx <- add_noise_Mx(noise$Mx, Corr.sample, n.cell, P.i)
-  O_CA <- add_noise_CA(O_CA, noise$CA)
-  O_IPM <- add_noise_IPM(O_IPM, U$lo, U$hi, noise$IPM)
-}
-
-
-########
-## Store samples
-########
-if(overwrite) {
-  saveRDS(O_Mx, here("vs", sp, paste0("O_Mx_", sampling.issue, ".rds")))
-  saveRDS(O_CA, here("vs", sp, paste0("O_CA_", sampling.issue, ".rds")))
-  saveRDS(O_IPM, here("vs", sp, paste0("O_IPM_", sampling.issue, ".rds")))
+for(s.i in samp.issues) {
+  ########
+  ## Impose sampling bias
+  ########
+  if(s.i=="geogBias") {
+    P.pr[P.i %in% geog.excl] <- 0
+  } else if(s.i=="sampBias") {
+    P.pr <- P.pr * env.in$rdLen[P.i]
+  }
+  
+  
+  ########
+  ## Draw samples
+  ########
+  Corr.sample <- map(1:n_samp, ~sample(P.i, O_n$Corr, replace=F, prob=P.pr))
+  Mech.sample <- map(1:n_samp, ~sample(P.i, O_n$Mech, replace=F, prob=P.pr))
+  
+  O_Mx <- map(Corr.sample, ~(1:n.cell %in% .))
+  O_CA <- sample_for_CA(S, Mech.sample, O_yr, prop.sampled)
+  O_IPM <- sample_for_IPM(S, Mech.sample, O_yr, prop.sampled)
+  
+  
+  ########
+  ## Impose sampling error
+  ########
+  if(s.i=="noise") {
+    O_Mx <- add_noise_Mx(noise$Mx, Corr.sample, n.cell, P.i)
+    O_CA <- add_noise_CA(O_CA, noise$CA)
+    O_IPM <- add_noise_IPM(O_IPM, U$lo, U$hi, noise$IPM)
+  }
+  
+  
+  ########
+  ## Store samples
+  ########
+  if(overwrite) {
+    saveRDS(O_Mx, here("vs", sp, paste0("O_Mx_", s.i, ".rds")))
+    saveRDS(O_CA, here("vs", sp, paste0("O_CA_", s.i, ".rds")))
+    saveRDS(O_IPM, here("vs", sp, paste0("O_IPM_", s.i, ".rds")))
+  }
 }
 
 
