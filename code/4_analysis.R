54165c7ff7d071882685b452533ba23a37009a97
Sz-Tim <tim.szewczyk@gmail.com>
2019-01-25 13:05:18 -0700

    TSS summary

--- code/4_analysis.R
+++ code/4_analysis.R
@@ -18,8 +18,8 @@ walk(dir("code", "fn", full.names=T), source)
 lam.df <- readRDS(here("vs", sp, "lam_df.rds"))
 
 f_P <- list.files(here("out", sp), pattern="P_", full.names=T)
-i_P <- extract_SDM_details(f_P)
-out <- map2(f_P, i_P, ~readRDS(.x) %>% mutate(SDM=.y[1], issue=.y[2])) %>%
+i_P <- extract_SDM_details(f_P, "P")
+out_P <- map2(f_P, i_P, ~readRDS(.x) %>% mutate(SDM=.y[1], issue=.y[2])) %>%
   do.call(bind_rows, .) %>%
   full_join(dplyr::select(lam.df, 
                           -one_of("x", "y", "x_y", "inbd", "lat", "lon", "id")), 
@@ -33,7 +33,17 @@ out <- map2(f_P, i_P, ~readRDS(.x) %>% mutate(SDM=.y[1], issue=.y[2])) %>%
                             lambda>=1 & prP<0.5 ~ "S:1 P:0",
                             lambda<1 & prP<0.5 ~ "S:0 P:0"))
 
+f_TSS <- list.files(here("out", sp), pattern="TSS_", full.names=T)
+i_TSS <- extract_SDM_details(f_TSS, "TSS")
+out_TSS <- rbind(map(f_TSS, ~readRDS(.x)[[1]]) %>%
+                   map2_dfr(., i_TSS, ~tibble(TSS=.x, Boundary="N", 
+                                              SDM=.y[1], issue=.y[2])), 
+                 map(f_TSS, ~readRDS(.x)[[2]]) %>%
+                   map2_dfr(., i_TSS, ~tibble(TSS=.x, Boundary="lam", 
+                                              SDM=.y[1], issue=.y[2])))
+
 if(overwrite) {
-  write_csv(out, here("out", sp, "out.csv"))
+  write_csv(out_P, here("out", sp, "out_P.csv"))
+  write_csv(out_TSS, here("out", sp, "out_TSS.csv"))
 }
 
