815e7bac1d36943fc5b69465a67fc8732fe11208
Tim Szewczyk <tim.szewczyk@gmail.com>
2019-04-12 07:29:47 -0600

    partitioning runs by SDM type for better parallelization

--- code/3_fitModels.R
+++ code/3_fitModels.R
@@ -10,10 +10,11 @@
 ## Setup
 ########
 # file specifications
-sp <- c("barberry", "garlic_mustard")[1]
+sp <- c("barberry", "garlic_mustard")[2]
 overwrite <- TRUE
-n_core_iss <- 4  # number of issues to run in parallel
-n_core_obs <- 6  # number of simulations to run in parallel for each issue
+SDMs <- c("MxE", "CAd", "IPM")[3]
+n_core_iss <- 2  # number of issues to run in parallel
+n_core_obs <- 3  # number of simulations to run in parallel for each issue
 n_sim <- 1 # number of simulations per sample (mechanistic only)
 
 # load workspace
@@ -82,7 +83,8 @@ foreach(i=seq_along(issue_i$Issue), .packages=c("dismo", pkgs)) %dopar% {
   
   # fit MaxEnt
   if("rJava" %in% rownames(installed.packages()) &&
-     issue %in% issue_i$Issue[c(1:4,8)]) {
+     issue %in% issue_i$Issue[c(1:4,8)] &&
+     "MxE" %in% SDMs) {
     P_MxE <- fit_MxE(sp_i$Num, issue, samp.issue, lam.df, v$Mx)
     if(overwrite) {
       saveRDS(P_MxE$diag, here(out.dir, paste0("Diag_MxE_", issue, ".rds")))
@@ -90,25 +92,31 @@ foreach(i=seq_along(issue_i$Issue), .packages=c("dismo", pkgs)) %dopar% {
       saveRDS(P_MxE$TSS_MxE, here(out.dir, paste0("TSS_MxE_", issue, ".rds")))
     }
   }
+  
   # fit CA-demographic
-  P_CA <- fit_CA(sp, sp_i, samp.issue, mod.issue, p,
-                 L$env_rct, L$env_rct_unscaled, lam.df, v$CA, m$CA,
-                 N_init, sdd.pr, sdd.ji, p.ji, n_sim, n_core_obs)
-  if(overwrite) {
-    saveRDS(P_CA$diag, here(out.dir, paste0("Diag_CAd_", issue, ".rds")))
-    saveRDS(P_CA$P_CAd, here(out.dir, paste0("P_CAd_", issue, ".rds")))
-    saveRDS(P_CA$TSS_CAd, here(out.dir, paste0("TSS_CAd_", issue, ".rds")))
+  if("CAd" %in% SDMs) {
+    P_CA <- fit_CA(sp, sp_i, samp.issue, mod.issue, p,
+                   L$env_rct, L$env_rct_unscaled, lam.df, v$CA, m$CA,
+                   N_init, sdd.pr, sdd.ji, p.ji, n_sim, n_core_obs)
+    if(overwrite) {
+      saveRDS(P_CA$diag, here(out.dir, paste0("Diag_CAd_", issue, ".rds")))
+      saveRDS(P_CA$P_CAd, here(out.dir, paste0("P_CAd_", issue, ".rds")))
+      saveRDS(P_CA$TSS_CAd, here(out.dir, paste0("TSS_CAd_", issue, ".rds")))
+    } 
   }
+  
   # fit IPM, CA-individual
-  P_IPM <- fit_IPM(sp, sp_i, samp.issue, mod.issue, p,
-                   L$env_rct_unscaled, lam.df, v$IPM, m$IPM, n_z, n_x,
-                   N_init, sdd.ji, p.ji, n_sim, n_core_obs)
-  if(overwrite) {
-    saveRDS(P_IPM$diag, here(out.dir, paste0("Diag_IPM_", issue, ".rds")))
-    saveRDS(P_IPM$P_CAi, here(out.dir, paste0("P_CAi_", issue, ".rds")))
-    saveRDS(P_IPM$P_IPM, here(out.dir, paste0("P_IPM_", issue, ".rds")))
-    saveRDS(P_IPM$TSS_IPM, here(out.dir, paste0("TSS_IPM_", issue, ".rds")))
-    saveRDS(P_IPM$TSS_CAi, here(out.dir, paste0("TSS_CAi_", issue, ".rds")))
+  if("IPM" %in% SDMs) {
+    P_IPM <- fit_IPM(sp, sp_i, samp.issue, mod.issue, p,
+                     L$env_rct_unscaled, lam.df, v$IPM, m$IPM, n_z, n_x,
+                     N_init, sdd.ji, p.ji, n_sim, n_core_obs)
+    if(overwrite) {
+      saveRDS(P_IPM$diag, here(out.dir, paste0("Diag_IPM_", issue, ".rds")))
+      saveRDS(P_IPM$P_CAi, here(out.dir, paste0("P_CAi_", issue, ".rds")))
+      saveRDS(P_IPM$P_IPM, here(out.dir, paste0("P_IPM_", issue, ".rds")))
+      saveRDS(P_IPM$TSS_IPM, here(out.dir, paste0("TSS_IPM_", issue, ".rds")))
+      saveRDS(P_IPM$TSS_CAi, here(out.dir, paste0("TSS_CAi_", issue, ".rds")))
+    }
   }
 }
 stopCluster(p.c)
