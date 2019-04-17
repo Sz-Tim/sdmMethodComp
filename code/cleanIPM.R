f8e1167e420a5e59dcff507adc4bc59e92a90512
Tim Szewczyk <tim.szewczyk@gmail.com>
2019-04-17 16:09:18 -0600

    script to remove partial files

---
+++ code/cleanIPM.R
@@ -0,0 +1,20 @@
+# file cleaning
+library(foreach)
+dirs <- paste0("out/sp1/", c("none/none/", "noise/none/", 
+                           "nonEq/none/", "sampBias/none/",
+                           "none/noSB/", "none/underDisp/",
+                           "none/overDisp/", "none/wrongCov/"))
+
+p.c <- makeCluster(8); registerDoSNOW(p.c)
+foreach(i=1:8) %dopar% {
+  d <- dirs[i]
+  fits <- dir(d, "IPM_fit", full.names=T)
+  if(length(fits)>0) {
+    for(f in 1:length(fits)) {
+      if("try-error" %in% class(try(readRDS(fits[f]), silent=TRUE))) {
+        file.remove(fits[f])
+      }
+    }
+  }
+}
+stopCluster(p.c)
