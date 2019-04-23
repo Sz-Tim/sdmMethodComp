f24da47419aae898ae848117cb1e5cbf28808dda
Tim Szewczyk <tim.szewczyk@gmail.com>
2019-04-23 07:28:12 -0600

    maxent diagnostics

--- code/4_analysis.R
+++ code/4_analysis.R
@@ -8,7 +8,7 @@
 ## Setup
 ########
 # file specifications
-sp <- "sp1"
+sp <- "sp2"
 overwrite <- TRUE
 
 # load workspace
@@ -17,6 +17,7 @@ suppressMessages(invisible(lapply(pkgs, library, character.only=T)))
 walk(dir("code", "fn", full.names=T), source)
 lam.df <- readRDS(here("vs", sp, "lam_df.rds"))
 
+# Aggregate predictions
 f_P <- list.files(here("out", sp), pattern="P_", full.names=T)
 i_P <- extract_SDM_details(f_P, "P")
 out_P <- map2(f_P, i_P, ~readRDS(.x) %>% mutate(SDM=.y[1], issue=.y[2])) %>%
@@ -33,6 +34,7 @@ out_P <- map2(f_P, i_P, ~readRDS(.x) %>% mutate(SDM=.y[1], issue=.y[2])) %>%
                             lambda>=1 & prP<0.5 ~ "S:1 P:0",
                             lambda<1 & prP<0.5 ~ "S:0 P:0"))
 
+# Calculate log likelihood
 out_LL <- out_P %>% 
   mutate(logP.N=dbinom(Surv.S>0, 1, prP, log=T),
          logP.N=ifelse(is.infinite(logP.N), -10, logP.N),
@@ -44,6 +46,7 @@ out_LL <- out_P %>%
   gather(Boundary, LogLik, 3:4)
 out_LL$Boundary <- factor(out_LL$Boundary, labels=c("lambda > 1", "N > 0"))
 
+# Aggregate TSS
 f_TSS <- list.files(here("out", sp), pattern="TSS_", full.names=T)
 i_TSS <- extract_SDM_details(f_TSS, "TSS")
 out_TSS <- rbind(map(f_TSS, ~readRDS(.)[[1]]) %>%
@@ -61,9 +64,36 @@ out_TSS <- rbind(map(f_TSS, ~readRDS(.)[[1]]) %>%
                                     Boundary="lambda > 1", 
                                     SDM=.y[1], issue=.y[2])))
 
+# Calculate MaxEnt diagnostics
+f_MxE <- list.files(here("out", sp), pattern="Diag_MxE", full.names=T)
+i_MxE <- extract_SDM_details(f_MxE, "Diag")
+diagRaw_MxE <- map(f_MxE, readRDS)
+AUC_MxE <- map2_dfr(diagRaw_MxE, i_MxE,
+                     ~tibble(AUC=c(map_dbl(.x[[1]], ~.@auc), 
+                                   map_dbl(.x[[2]], ~.@auc)),
+                             Boundary=rep(c("N > 0", "lambda > 1"), each=100),
+                             issue=.y[2]))
+ROC_MxE <- rbind(map2_dfr(diagRaw_MxE, i_MxE,
+                          ~tibble(TPR=unlist(map(.x[[1]], ~.@TPR)),
+                                  FPR=unlist(map(.x[[1]], ~.@FPR)),
+                                  obs=rep(1:100, times=map_int(.x[[1]], 
+                                                               ~length(.@TPR))),
+                                  issue=.y[2],
+                                  Boundary="N > 0")),
+                 map2_dfr(diagRaw_MxE, i_MxE,
+                          ~tibble(TPR=unlist(map(.x[[2]], ~.@TPR)),
+                                  FPR=unlist(map(.x[[2]], ~.@FPR)),
+                                  obs=rep(1:100, times=map_int(.x[[2]], 
+                                                               ~length(.@TPR))),
+                                  issue=.y[2],
+                                  Boundary="lambda > 1")))
+
+
 if(overwrite) {
   write_csv(out_P, here("out", sp, "out_P.csv"))
   write_csv(out_LL, here("out", sp, "out_LL.csv"))
   write_csv(out_TSS, here("out", sp, "out_TSS.csv"))
+  write_csv(AUC_MxE, here("out", sp, "out_AUC.csv"))
+  write_csv(ROC_MxE, here("out", sp, "out_ROC.csv"))
 }
 
